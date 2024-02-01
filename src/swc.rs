b739a6a343254d71f5922e9a16780d607b594c61
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2024-02-01 19:30:27 +0000

    More ergonomic read/write swc

--- src/swc.rs
+++ src/swc.rs
@@ -1,10 +1,8 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 
 pub use swc_neuron;
-use swc_neuron::SampleId;
-use swc_neuron::SwcParseError;
 use swc_neuron::{structures::StructureIdentifier, SwcSample};
-use thiserror::Error;
+use swc_neuron::{Header, SampleId, SwcLines};
 
 use crate::error::{EdgeBuild, IdAbsent, InvalidId};
 use crate::spatial::{Location, Point, UpdateLocation};
@@ -46,26 +44,43 @@ impl<S: StructureIdentifier> UpdateLocation<3> for SwcData<S> {
 //     Swc(#[from] SwcParseError),
 // }
 
-// pub fn read_swc<S: StructureIdentifier, R: BufRead>(
-//     reader: R,
-// ) -> Result<Tree<SwcData<S>, usize>, SwcReadError> {
-//     SwcLines::<S, R>::new(reader)?.filter()
-// }
+pub fn read_swc<S: StructureIdentifier, R: BufRead>(
+    reader: R,
+) -> Result<Tree<SwcData<S>, SampleId>, String> {
+    let mut it = SwcLines::<S, R>::new(reader)
+        .map_err(|_e| "could not read header".to_string())?
+        .filter_map(|res| {
+            let ln = match res {
+                Ok(ln) => ln,
+                Err(e) => return Some(Err(e)),
+            };
+            match ln {
+                swc_neuron::SwcLine::Comment(_c) => None,
+                swc_neuron::SwcLine::Sample(s) => Some(Ok(s)),
+            }
+        });
 
-pub fn swc_to_tree<S: StructureIdentifier, T: IntoIterator<Item = SwcSample<S>>>(
-    samples: T,
-) -> Result<Tree<SwcData<S>, SampleId>, EdgeBuild<SampleId>> {
-    let mut it = samples.into_iter();
-    let r = it.next().ok_or(EdgeBuild::NoRoot)?;
-    if let Some(p) = r.parent_id {
-        return Err(InvalidId::Absent(IdAbsent::from(p)).into());
+    let r = it
+        .next()
+        .ok_or("no root".to_string())?
+        .map_err(|e| e.to_string())?;
+
+    if let Some(_p) = r.parent_id {
+        return Err("id absent".into());
     }
+
     let mut t = Tree::new(r.sample_id, (&r).into());
-    for s in it {
+    for res in it {
+        let s = match res {
+            Ok(s) => s,
+            Err(e) => return Err(e.to_string()),
+        };
         let p = s
             .parent_id
-            .ok_or(EdgeBuild::MultipleRoots(vec![s.sample_id, r.sample_id]))?;
-        t.add_child(p, s.sample_id, (&s).into())?;
+            .ok_or(EdgeBuild::MultipleRoots(vec![s.sample_id, r.sample_id]))
+            .map_err(|e| e.to_string())?;
+        t.add_child(p, s.sample_id, (&s).into())
+            .map_err(|e| e.to_string())?;
     }
     Ok(t)
 }
@@ -86,12 +101,20 @@ impl<S: StructureIdentifier> HasSwcData<S> for SwcData<S> {
     }
 }
 
-pub fn tree_to_swc<S: StructureIdentifier, D: HasSwcData<S>>(
+pub fn write_swc<S: StructureIdentifier, D: HasSwcData<S>, W: Write, H: Header>(
     tree: &Tree<D, SampleId>,
-) -> impl Iterator<Item = SwcSample<S>> + '_ {
-    tree.dfs_edges(*tree.root()).unwrap().map(|(p, c, d)| {
+    header: Option<H>,
+    mut writer: W,
+) -> io::Result<()> {
+    if let Some(h) = header {
+        for line in h.to_string().lines() {
+            write!(writer, "#")?;
+            writeln!(writer, "{}", line)?;
+        }
+    }
+    for (p, c, d) in tree.dfs_edges(*tree.root()).unwrap() {
         let loc = d.location();
-        SwcSample {
+        let sample = SwcSample {
             sample_id: c,
             structure: d.structure(),
             x: loc[0],
@@ -99,6 +122,8 @@ pub fn tree_to_swc<S: StructureIdentifier, D: HasSwcData<S>>(
             z: loc[2],
             radius: d.radius(),
             parent_id: p,
-        }
-    })
+        };
+        writeln!(writer, "{}", sample.to_string())?;
+    }
+    Ok(())
 }
